1ee5e43cc189e73bf445e565ab1433be3238d66c
charlesmoreno020 <charlesmelvinmoreno@gmail.com>
2026-05-11 04:21:51 +0000

    Day 5

---
+++ Toy.java
@@ -0,0 +1,23 @@
+public class Toy {
+    public String name ;
+    public String brand; 
+    public double price; 
+    public int quantity; 
+
+    public static void main (String [] args ) {
+        System.out.println("Hello Java Trainees"
+
+        );
+    
+    
+
+
+
+        
+    }
+}
+
+
+
+
+
