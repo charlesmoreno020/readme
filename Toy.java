04f7621c6f2716f457ebeac9c4de86beeccf641e
charlesmoreno020 <charlesmelvinmoreno@gmail.com>
2026-05-12 04:10:34 +0000

    Day 6

--- Toy.java
+++ Toy.java
@@ -1,13 +1,26 @@
 public class Toy {
-    public String name ;
-    public String brand; 
+    public String name = "Lego" ;
+    public String brand = "LEGO Company" ; 
     public double price; 
     public int quantity; 
 
-    public static void main (String [] args ) {
-        System.out.println("Hello Java Trainees"
 
-        );
+// set the amount of the toy to the argument example setPrice(1000) //
+    void setPrice (double price) {this.price = price;
+
+    }
+
+    // psvm //
+    public static void main (String [] args ) {
+
+
+        System.out.println("Hello Java Trainees");
+
+        Toy toy1 = new Toy();
+        System.out.println(toy1.name);
+
+
+        System.out.println(toy1.brand);
     
     
 
